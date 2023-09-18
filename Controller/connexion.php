362932cbda9b8f81a35df2bb377f20906b3eef3d
Elite <nathan.strady@sfr.fr>
2023-09-18 12:21:09 +0200

    Création de la page connexion + gestion de la connexion

--- Controller/connexion.php
+++ Controller/connexion.php
@@ -1,1 +1,18 @@
 <?php
+function connection($login, $password)
+{
+    if (isset($_POST['login']) && isset($_POST['password'])) {
+        if ($_POST["login"] == $login && $_POST["password"] == $password) {
+            session_start();
+            $_SESSION["login"] = $_POST['login'];
+            $_SESSION["password"] = $_POST['password'];
+            header("location: ../View/Accueil.php");
+        } else {
+            echo "CONNECTION IMPOSSIBLE : MAUVAIS LOGIN OU MOT DE PASSE !";
+        }
+    }
+}
+
+connection('nathan','1234')
+
+?>

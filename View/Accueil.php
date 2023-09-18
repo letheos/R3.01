7c707416c33072a82681c2e5b16e71fb779f81d7
Elite <nathan.strady@sfr.fr>
2023-09-18 12:21:16 +0200

    Création de la page connexion + gestion de la connexion

---
+++ View/Accueil.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+    <h1> Accueil </h1>
+
+</body>
+</html>
---
+++ View/PageConnexion.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+    <title> Connection !  </title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+<h1> Connexion </h1>
+
+<form method="post" action="../Controller/connexion.php"
+<div class="champ">
+    <input type="text" id="login" name="login" placeholder="login"> <br>
+    <input type="password" id="password" name="password" placeholder="password" > <br>
+</div>
+<div class="button">
+    <button type="submit" id="submit" name="submit"> Connexion </button>
+</div>
+</body>
+</html>

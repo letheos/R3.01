4f0ff30a182706eb0ba773c3e506991f91a5a26a
Elite <nathan.strady@sfr.fr>
2023-09-19 10:49:03 +0200

    Ajout du php de la page de connection Ajout des requête sql et de la connection à la base de donnée pour la connection

--- Controller/connexion.php
+++ Controller/connexion.php
@@ -1,18 +1,25 @@
 <?php
-function connection($login, $password)
+
+include '../Model/Database.php';
+function connection()
 {
     if (isset($_POST['login']) && isset($_POST['password'])) {
-        if ($_POST["login"] == $login && $_POST["password"] == $password) {
-            session_start();
-            $_SESSION["login"] = $_POST['login'];
-            $_SESSION["password"] = $_POST['password'];
-            header("location: ../View/Accueil.php");
+        $conn = conn();
+        if (isLoginExist($conn, $_POST['login'])) {
+            if (searchUser($conn, $_POST['login'], $_POST['password'])) {
+                session_start();
+                $_SESSION["login"] = $_POST['login'];
+                $_SESSION["password"] = $_POST['password'];
+                header("location: ../View/Accueil.php");
+            } else {
+                echo "Vous avez renseigné le mauvais mot de passe";
+            }
         } else {
-            echo "CONNECTION IMPOSSIBLE : MAUVAIS LOGIN OU MOT DE PASSE !";
+            echo "Vous avez renseigné le mauvais Login";
         }
     }
 }
 
-connection('nathan','1234')
+connection();
 
 ?>
--- Model/Database.php
+++ Model/Database.php
@@ -1,1 +1,25 @@
 <?php
+
+//Fonction de connexion à la base de donnée
+function conn(){
+     $bdd = new PDO("mysql:host=localhost;dbname=BDD interne", 'root', 'root');
+     return $bdd;
+}
+
+//Vérification du login
+function isLoginExist($conn, $login){
+    $req = $conn->prepare("SELECT login FROM Identification WHERE login = ?");
+    $req->execute(array($login));
+    $result = $req->fetch();
+    return $result != null;
+}
+
+//Vérification de l'utilisateur
+function searchUser($conn, $login, $password){
+    $req = $conn->prepare("SELECT login, password from Identification WHERE login = ? AND password = ?");
+    $req->execute(array($login, $password));
+    $result = $req->fetch();
+
+    return $result['login'] != null && $result['password'] != null;
+
+}

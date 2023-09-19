a7daa8848f7c7d0155cb8a4a1a2a8be2ed2f849e
Thomascbn <th0mas1303@hotmail.fr>
2023-09-19 10:47:06 +0200

    Ajout du visuel de la page réinitialisation

---
+++ View/Reinitialisation.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SAE Réinitialisation</title>
+</head>
+
+<body>
+<h2>Mot de passe oublié</h2>
+<form method="post">
+    <div class="container">
+        <label for="email"><b>Email</b></label>
+        <input type="email" placeholder="Entrez votre adresse mail" name="email" required>
+        <button type="submit">Envoyez moi un nouveau mot de passe</button>
+    </div>
+</form>
+</body>
+
+</html>
+

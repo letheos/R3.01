cc9f8661aef0ae948f163d1fe4c7eb5c638720e5
rafale2000 <loick1morneau@icloud.com>
2023-10-19 17:19:44 +0200

    transformation du code, passage de sorte de php echo html a html dure

--- View/pageDesactiveActiveEtudiant.php
+++ View/pageDesactiveActiveEtudiant.php
@@ -1,7 +1,9 @@
 <?php
 require_once "../Controller/ControllerActiveDesactiveCompte.php";
+require_once "../Model/ModelActivationDesactivationCompte.php";
 //include "../Controller/ControllerActiveDesactiveCompte.php";
-$conn = require "../Model/Database.php"
+$conn = require "../Model/Database.php";
+
 ?>
 
 <!doctype html>
@@ -16,7 +18,6 @@ $conn = require "../Model/Database.php"
 </head>
 
 
-
 <body>
 <section class="haut">
     <div class="titre">
@@ -29,6 +30,44 @@ $conn = require "../Model/Database.php"
     </div>
 </section>
 
+<?php
+$results = getAllStudents();
+
+foreach ($results as $row) {
+    ?>
+
+    <section class="affichEtu">
+        <div class="rounded-box">
+            <form method="post" action="../Controller/ControllerActiveDesactiveCompte.php">
+                <!--input de nom à faire --><input name="name" type="hidden" value="'.$row['name'].'">
+                <input name="firstname" type="hidden" value=" <?= $row['3'] ?>">
+                <input name="lastname" type="hidden" value=" <?= $row['name'] ?>">
+                <p> nom : <?= $row['name'] ?></p>
+                <p> prénom : <?= $row[3] ?></p>
+                <button id="delete" name="delete" type="button">supprime</button>
+                <?php
+                if ($row[7] == 1) { ?>
+
+                    <button class="desactive"  name="bool" value="0" type="submit"> désactive</button>
+                    <p class="notSearch">n'est pas en recher active</p>
+                    <?php
+                } else {
+                    ?>
+                    <button class="active" name="bool" value="1" type="submit"> active</button>
+                    <p class="isSearch">est en recherche active</p>
+                    <?php
+                }
+                ?>
+
+            </form>
+        </div>
+    </section>
+    <?php
+}
+//
+?>
+
+
 <form method="POST" action=" ">
 
     <section class="filtreCandidats">
@@ -37,7 +76,7 @@ $conn = require "../Model/Database.php"
             <label for="formation" class="form-select-label"> Formation </label>
             <?php
 
-            getAllFormation();
+            //getAllFormation();
 
 
             ?>
@@ -46,7 +85,8 @@ $conn = require "../Model/Database.php"
         </div>
 
         <div class="formName">
-            <input type="text" class="form-control" name="nameCandidates" id="nameCandidates" placeholder="Filtrage par nom">
+            <input type="text" class="form-control" name="nameCandidates" id="nameCandidates"
+                   placeholder="Filtrage par nom">
         </div>
         <div class="checkbox">
             <input class="form-check" type="checkbox" name="isActive" id="isActive">
@@ -60,23 +100,10 @@ $conn = require "../Model/Database.php"
 </form>
 
 
-<section class="donnees">
-
-
-
-    <div class="affiche">
-        <?php
-        listAffichageSelect();
-        ?>
-    </div>
-
-</section>
-
-
 <section class="bas">
 
     <footer>
-        <div class="nomFooter" >
+        <div class="nomFooter">
             <p>
                 Timothée Allix, Nathan Strady, Theo Parent, Benjamin Massy, Loïck Morneau
 

a6945f8648cc26f5751d23fe2cc22166b1f85d78
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-10-22 16:51:23 +0200

    Update jsCreationTableau.js

---
+++ Controller/jsCreationTableau.js
@@ -0,0 +1,44 @@
+/**
+ * Fichier js qui gère l'ajout et la suppression de champs dans la page Creation pageCreationTableau
+ * @author Loïcl Morneau
+ * */
+
+//Variable générale nécessaire pour la récupération de certaine donnée et l'initialisation des compteurs
+const limitChamp = 4;
+
+document.addEventListener("DOMContentLoaded", function (){
+    var slider = document.getElementById("myRange");
+    var valeurOutput = document.getElementById("valeurSlider");
+
+// Mettre à jour la valeur initiale
+    valeurOutput.innerHTML = slider.value;
+
+// Définir un événement pour détecter les changements de la valeur du slider
+    slider.addEventListener("input", function() {
+        valeurOutput.innerHTML = this.value;
+    });
+})
+
+function addAdress(){
+    if(nbChampAdress < limitChamp){
+        const newDiv = document.createElement("div");
+        newDiv.className = "truc";
+
+        newDiv.innerHTML = `
+            <div id="city"> ville
+                <label for="city">
+                    <select name="city">
+                        <option value="Lille">Lille</option>
+                        <option value="Valenciennes">Valenciennes</option>
+                    </select>
+                </label>
+            </div>
+
+            <div class="slidecontainer" >
+                <label for="myRange">Distance de recherche :</label>
+                <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+                <p>Valeur : <span id="valeurSlider">50</span></p>
+        </div>
+        `;
+    }
+}

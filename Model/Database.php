c58339f0b34041dccffda4c387e258cc806ca7ce
Elite <nathan.strady@sfr.fr>
2023-10-23 15:32:13 +0200

    Ajout du js permettant la suppression d'un candidat, correction du bouton détail permettant l'affichage en détail d'un candidat.

--- Model/Database.php
+++ Model/Database.php
@@ -1,6 +1,6 @@
 <?php
 //Fichier des créations de la connexion
-$conn = new PDO("mysql:host=localhost;dbname=localDatabaseTest3", "root", "root");
+$conn = new PDO("mysql:host=localhost;dbname=localDatabase2", "root", "root");
 return $conn
 ?>
 

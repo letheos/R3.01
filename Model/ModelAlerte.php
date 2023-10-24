90ee7cfaa679b2fe8ef29f73668d3cf01515bb43
TimotheeAllix <129874545+TimotheeAllix@users.noreply.github.com>
2023-10-24 11:53:22 +0200

    Add files via upload
    Alertes fonctionnelles, commit manuel car git planté sur phpstorm

--- Model/ModelAlerte.php
+++ Model/ModelAlerte.php
@@ -10,7 +10,7 @@
 
 
 function hasPastAlert($conn,$login){
-    $req = $conn->prepare("SELECT COUNT(idalert) FROM alert WHERE login = ? AND remindAT<CURRENT_DATE");
+    $req = $conn->prepare("SELECT COUNT(idalert) FROM alert WHERE login = ? AND remindAT<CURRENT_DATE and seen= false");
     try {
         $req->execute(array($login));
         $result = $req->fetchAll();
@@ -26,20 +26,30 @@ function hasPastAlert($conn,$login){
  * @param $conn
  * @param $login
  * @return Exception|mixed|PDOException
- * Fonction qui récupère toutes les alertes concernant un utilisateur donné
+ * Fonction qui récupère toutes les alertes passées et les met en "seen" concernant un utilisateur donné
  */
-/*function selectPastAlert($conn, $login){
+function selectPastAlert($conn, $login){
     try {
-        $sql = 'SELECT idAlert,note FROM Alert WHERE login = ? AND remindAT<CURRENT_TIMESTAMP' ;
+        $sql = 'SELECT note,remindAt,idAlert FROM Alert WHERE login = ? AND remindAT<CURRENT_TIMESTAMP' ;
         $req = $conn->prepare($sql);
         $req->execute(array($login));
     }
     catch (PDOException $e){
         return $e;
     }
+    foreach ($req as $row){
+        setAlertSeen($conn,$row[2]);
+    }
     return $req->fetchAll();
-}*/
+}
 
+/**
+ * @param $conn
+ * @param $login
+ * @param $future
+ * @return Exception|mixed|PDOException
+ * Selectionne la liste des alertes d'un utilisateurs selon un parametre future determinant si l'on filtre les anlertes futures ou non
+ */
 function selectAlert($conn,$login,$future){
     try {
         $sql = "SELECT IDAlert,note,remindAt FROM alert WHERE login = ? ";
@@ -97,6 +107,12 @@ function ajouterAlerte($conn,$login,$remindAt,$note)
     return 1;
 }
 
+/**
+ * @param $conn
+ * @param $id
+ * @return Exception|PDOException|void
+ * Cette fonction permet de supprimer une alerte
+ */
 function supprimerAlerte($conn,$id){
     try {
         $requete = "DELETE from alert WHERE idalert=?";

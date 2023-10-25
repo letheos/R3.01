76d187adb2101b8393646dd159949d2771cbc4ec
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-10-25 16:43:06 +0200

    ajout de 2 function qui prenne des select *

--- Controller/controllerCreationTableau.php
+++ Controller/controllerCreationTableau.php
@@ -1,1 +1,16 @@
 <?php
+require "../Model/modelCreationTableau.php";
+/**
+ * @param $conn PDO
+ * @return mixed
+ * take a PDO connection and return the values of getAllParcours
+ */
+function controllerGetAllParcours(){
+    $conn = require "../Model/database.php";
+    return getAllParcours($conn);
+}
+
+function controllerGetAllFormations(){
+    $conn = require "../Model/database.php";
+    return getAllFormation($conn);
+}

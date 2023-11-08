57d7cec4c9cec7f05ef8e189273cc73c02666232
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-11-08 12:23:21 +0100

    ajout de nouvelle méthode

--- Model/ModelCreationTableau.php
+++ Model/ModelCreationTableau.php
@@ -12,15 +12,30 @@ $conn = require "Database.php";
  * @param $conn
  * @return mixed
  */
-
 function getStudentsWithConditions($isActif,$isPermis,$year,$formation,$parcours,$conn){
+    if()
     //problème vient quand je mes des conditions
     $sql = "SELECT * FROM candidate join candidateaddress USING(idCandidate) WHERE yearOfFormation =(?) AND isInActiveSearch =(?)  AND permisB =(?)";
     $req = $conn->prepare($sql);
-    $req->execute($year,$isActif,$isPermis);
+    $params = array($year, $isActif, $isPermis);
+    $req->execute($params);
     return $req->fetchall();
     //AND city =(?) AND radius >=(?)
 }
+
+function getStudentTest($isActif,$isPermis,$year,$formation,$parcours,$conn,$ine){
+    if($isPermis){
+        $sql = "SELECT () FROM candidate join candidateaddress USING(idCandidate) WHERE yearOfFormation =(?) AND isInActiveSearch =(?)  AND permisB =(?)";
+    }
+        //problème vient quand je mes des conditions
+        $sql = "SELECT * FROM candidate join candidateaddress USING(idCandidate) WHERE yearOfFormation =(?) AND isInActiveSearch =(?)  AND permisB =(?)";
+    $req = $conn->prepare($sql);
+    $params = array($year, $isActif, $isPermis);
+    $req->execute($params);
+    return $req->fetchall();
+
+}
+
 
 /**
  * @param $conn PDO
@@ -33,11 +48,43 @@ function getAllParcours($conn){
     $req->execute();
     return $req->fetchall();
 }
+//faire avec condition
 
+/**
+ * @param $conn PDO
+ * @param $parcours String
+ * @return mixed
+ * take a PDO connexion and an educative parcours and return all the parcours in the database that correspond
+ */
+function getParcoursWithConditions($conn,$parcours){
+    $sql = "SELECT * FROM parcours WHERE nameParcours LIKE(?)";
+    $req = $conn->prepare($sql);
+    $req->execute($parcours);
+    return $req->fetchall();
+}
+
+/**
+ * @param $conn PDO
+ * @return mixed
+ * take a PDO connexion and return all the formation in the databse
+ */
 function getAllFormation($conn){
     $sql = "SELECT * FROM formation";
     $req = $conn->prepare($sql);
     $req->execute();
+    return $req->fetchall();
+}
+
+/**
+ * @param $conn PDO
+ * @param $formation String
+ * @return Array[String]
+ * return
+ */
+function getFormationWithCoditions($conn, $formation){
+    $sql = "SELECT * FROM formation WHERE nameFormation LIKE(?)";
+    $req = $conn->prepare($sql);
+    $req->execute($formation);
     return $req->fetchall();
 }
 

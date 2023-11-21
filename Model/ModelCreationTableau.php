131c7118c05110762a984d52670dbd2da8bb7fab
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-11-21 16:45:27 +0100

    mise à jour de insertNewDashBoard

--- Model/ModelCreationTableau.php
+++ Model/ModelCreationTableau.php
@@ -230,17 +230,38 @@ function getFormationWithCoditions($conn, $formation)
  * @param $isPhone boolean
  * @param $idUser string //a trouver comment récupérer l'user pour pouvoir l'enregistré dans la bdd
  * @param $conn PDO //the connection at the database
- * @return void
+ * @return boolean
  * this funtion insert à new tableau de bord in the database is link it whit an user
  */
-function insertNewDashBoard($isPermis, $year, $formation, $parcours, $isIne, $isAddress, $isPhone, $idUser,$conn){
+function insertNewDashBoard($isPermis, $year, $formation, $parcours, $isIne, $isAddress, $isPhone, $idUser, $conn){
     $user = getUserWithId($idUser, $conn);
-    $sql = "SELECT * FROM formation WHERE nameFormation LIKE(?)";
+    $sql = "INSERT INTO dashBoard (isPermis, yearOfFormation, formation, parcours, isIne, isAddress, isPhone) VALUES(?,?,?,?,?,?,?);";
     $req = $conn->prepare($sql);
-    $req->execute($formation);
-    return $req->fetchall();
+    $params = array($isPermis, $year, $formation, $parcours, $isIne, $isAddress, $isPhone);
+
+    try {
+        $req->execute($params);
+        // Omit the fetchall() line if you don't expect a result set.
+        // return $req->fetchAll();
+        return true; // Indicate success if no exception was thrown.
+    } catch (PDOException $e) {
+        // Handle the exception, e.g., log the error or return false.
+        // echo "Error: " . $e->getMessage();
+        return false;
+    }
 }
 
+/*
+function insertNewDashBoard($isPermis, $year, $formation, $parcours, $isIne, $isAddress, $isPhone, $idUser,$conn){
+    $user = getUserWithId($idUser, $conn);
+    $sql = "INSERT INTO dashBoard (isPermis, yearOfFormation, formation, parcours, isIne, isAddress, isPhone) VALUES(?,?,?,?,?,?,?);";
+    $req = $conn->prepare($sql);
+$params = array( $isPermis, $year, $formation, $parcours, $isIne, $isAddress, $isPhone);
+    $req->execute($params);
+    //$req->execute($isPermis, $year, $formation, $parcours, $isIne, $isPhone);
+    return $req->fetchall();
+}
+*/
 /**
  * @param $idUser String
  * @param $conn PDO

d781937e50f0b93e0d0fead5eda7f1b1b834ba03
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-12-08 10:22:41 +0100

    ajout de un l a ControllerAfficheTableau.php et supprissions du code en commantaire

--- Controller/ControllerAfficheTableau.php
+++ Controller/ControllerAfficheTableau.php
@@ -1,36 +1,8 @@
 <?php
 require "../Model/ModelSelect.php";
 $conn = require "../Model/Database.php";
-/*
-if(isset($_POST['validate'])){
-    // toute les valeurs
-    echo '<script>alert("a")</script>';
 
-    if(isset($_POST["formations"]) and isset($_POST["parcours"]) and isset($_POST["formAnnee"]) and isset($_POST["isPermis"]) ){
-        if(isset($_POST["isPhone"])){$isPhone = 1;}
-        if(isset($_POST["isIne"])){$isIne = 1;}
-        if(isset($_POST["isAddress"])){$isAddress = 1;}
-        if(!(isset($_POST["isPhone"]))){$isPhone = 0;}
-        if(!(isset($_POST["isIne"]))){$isIne = 0;}
-        if(!(isset($_POST["isAddress"]))){$isAddress = 0;}
-
-        controllergetStudentsWithConditions($_POST['isPermis'],$_POST['formAnnee'],$_POST['formations'],$_POST['parcours'],$conn,$isIne,$isAddress,$isAddress);
-        header("Location: ../View/PageAfficheTabeau.php");
-
-    }
-}
-
-
-
-function controllergetStudentsWithConditions($isPermis, $year ,$formation, $conn ,$parcours ,$ine ,$address ,$phone){
-    //trouver coment envoyer les paramètres
-
-    return getStudentsWithConditions($isPermis,$year,$formation,$conn,$parcours,$ine,$address,$phone);
-}
-return getStudentsWithConditions($_POST['isPermis'],$_POST['formAnnee'],$_POST['formations'],$_POST['parcours'],$conn,$_POST['isIne'],$_POST['isIne'],$_POST['isPhone']);
-
-*/
-function ControlerGetDashBoardPerUser($login){
+function ControllerGetDashBoardPerUser($login){
     $conn = require "../Model/Database.php";
     return getDashBoardPerUser($login,$conn);
 }

c9e63154c6b79c87a3a22919cc330ce99cc10631
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-12-13 12:27:48 +0100

    changement pour le bouton qui cache les informations

--- Controller/JsDisplayDashBoard.js
+++ Controller/JsDisplayDashBoard.js
@@ -5,15 +5,23 @@
 /**
  * try to show or hide the element in the div elementCacher (nom provisiore)
  */
+
 function affiche(){
     var divCacher = document.getElementById("elementCacher")
-    var button = document.getElementById("buttonShowhide")
+
+    let cache = document.getElementById('btnCache');
 
     if(divCacher.style.display == "none"){
         divCacher.style.display = "block"
-        button.innerText("-")
+
+
+
     } else{
         divCacher.style.display = "none"
-        button.innerText("+")
     }
+    if (cache.value == '-')
+        cache.value = '+'
+    else
+        cache.value = '-'
 }
+
--- View/PageAfficheTableau.php
+++ View/PageAfficheTableau.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['login'])) {
 }
 else{
     session_start();
-    $_SESSION['login'] = "utilisateur2";
+    $_SESSION['login'] = "user1";
     $User = $_SESSION['login'];
 }
 
@@ -17,17 +17,17 @@ else{
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="StylePageAfficheTableau.css">
+    <title>Page affiche les tableaux de bord</title>
 </head>
 <body>
 
 <header class="banner">
     <form>
         <h1 class="TexteProfil">
-            Affichage des tableau de bord
+            Affichage des tableaux de bord
         </h1>
         <button class="btn btn-light" type="submit" name="retourAccueil"
                 onclick="window.location.href='PageAccueil.php'">Retour à l'accueil
@@ -35,51 +35,43 @@ else{
     </form>
 </header>
 
-</body>
-</html>
-
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="StylePageAfficheTableau.css">
-    <title>PageAfficheDashBoard</title>
-</head>
-<p>
-
 
     <?php
-    $dashBoards = ControlerGetDashBoardPerUser($_SESSION['login']);
-
+    $dashBoards = ControllerGetDashBoardPerUser($_SESSION['login']);
+    //récup les id de dashboard
+    //récup les formation année etc
+    print_r($dashBoards);
+    $i = 0;
     foreach ($dashBoards as $dashBoard) {
+        $i+=1;
     //print_r($dashBoard);
     foreach ($dashBoard as $valeur) {
     ?>
-<div class="rounded-box">
+<div class="rounded-box" >
     <li><?= $valeur[0] ?>   </li>
-    <li style="display: none">le tableau de bord numéro <?= $valeur[1] ?>   </li>
+    <div id="elementCacher" style="display: block">
+    <li >le tableau de bord numéro <?= $valeur[1] ?>   </li>
     <?php if ($valeur[2]) {
-        echo '<li style="display: none">ont le permis</li>';
+        echo '<li>ont le permis</li>';
     } else {
-        echo "<li style='display: none' >n'ont pas le permis</li>";
+        echo "<li>n'ont pas le permis</li>";
     } ?>
-    <li style="display: none"> année d'étude : <?= $valeur[3] ?>   </li>
-    <li style="display: none"> formation : <?= $valeur[4] ?>   </li>
-    <li style='display: none'>>recherche les étudiants ayant comme parcours : <?= $valeur[5] ?>   </li>
-    <?php echo $valeur[6] ? "<li style='display: none'> INE affiché</li>" : "<li style='display: none'> INE non affiché</li>"; ?>
-    <?php echo $valeur[7] ? "<li style='display: none'>adresse affiché</li>" : "<li style='display: none'> adresse non affiché</li>"; ?>
-    <?php echo $valeur[8] ? "<li style='display: none'>numéro de téléphone affiché</li>" : "<li style='display: none'> numéro de téléphone non affiché</li>"; ?>
 
-    <button > <img src="C:\document\but2\sae\stest.png" alt="+" /> </button>
-    <form <!-- mettre en action la fonction supprimer -->>
+    <?php echo $valeur[3] ? "<li> INE affiché</li>" : "<li > INE non affiché</li>"; ?>
+    <?php echo $valeur[4] ? "<li>adresse affiché</li>" : "<li > adresse non affiché</li>"; ?>
+    <?php echo $valeur[5] ? "<li>numéro de téléphone affiché</li>" : "<li > numéro de téléphone non affiché</li>"; ?>
+    </div>
+    <script src="../Controller/JsDisplayDashBoard.js"></script>
+
+    <input onclick="affiche()" type="button" value="-" id="btnCache<?=$i?>">
+
+
+
+    <form >
+        <!-- mettre en action la fonction supprimer -->
         <button>supprimer</button>
-    </form>
 
-    <form <!-- mettre en action la fonction modifier -->>
+        <!-- mettre en action la fonction modifier -->
         <button>modifier</button>
     </form>
 </div>

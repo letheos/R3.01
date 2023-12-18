4693af5828f83c5c4fe5ab9819f688394c4ddb32
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-12-18 16:44:17 +0100

    Ajout de la fonction supprimer

--- View/PageAfficheTableau.php
+++ View/PageAfficheTableau.php
@@ -4,8 +4,7 @@ require "../Controller/ControllerAfficheTableau.php";
 
 if (isset($_SESSION['login'])) {
     $User = $_SESSION['login'];
-}
-else{
+} else {
 
     $_SESSION['login'] = "user1";
     $User = $_SESSION['login'];
@@ -17,7 +16,8 @@ else{
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="StylePageAfficheTableau.css">
     <title>Page affiche les tableaux de bord</title>
@@ -33,64 +33,71 @@ else{
                 onclick="window.location.href='PageAccueil.php'">Retour à l'accueil
         </button>
     </form>
+
+    <form action="../View/PageCreationTableau.php">
+        <button id="createDashboard">Ajouter un tableau de bord</button>
+    </form>
 </header>
 
 <section class="theDashBoards">
     <?php
     $dashBoards = ControllerGetDashBoardPerUser($_SESSION['login']);
-   $i = 0;
+    $i = 0;
+
     foreach ($dashBoards as $dashBoard) {
-        $i+=1;
 
-        foreach ($dashBoard as $valeur) {?>
+        $i += 1;
 
-    <div class="rounded-box" >
-        <li> <?= $valeur[0] ?>   </li>
+        foreach ($dashBoard as $valeur) { ?>
 
-        <div id= <?= $i ?> style="display: block">
-        <li >le tableau de bord numéro <?= $valeur[1] ?>   </li>
-    <?php
-    echo $valeur[2] ? "<li>ont le permis</li>" : "<li>n'ont pas le permis</li>";
-    echo $valeur[3] ? "<li> INE affiché</li>" : "<li > INE non affiché</li>";
-    echo $valeur[4] ? "<li>adresse affiché</li>" : "<li > adresse non affiché</li>";
-    echo $valeur[5] ? "<li>numéro de téléphone affiché</li>" : "<li > numéro de téléphone non affiché</li>"; ?>
+            <div class="rounded-box">
+                <li> <?= $valeur[0] ?>   </li>
 
-
-    </div>
-            <script src="../Controller/JsDisplayDashBoard.js"></script>
+                <div id= <?= $i ?> style="display:block">
+                    <li>le tableau de bord numéro <?= $valeur[1] ?>   </li>
+                    <?php
+                    echo $valeur[2] ? "<li>ont le permis</li>" : "<li>n'ont pas le permis</li>";
+                    echo $valeur[3] ? "<li> INE affiché</li>" : "<li > INE non affiché</li>";
+                    echo $valeur[4] ? "<li>adresse affiché</li>" : "<li > adresse non affiché</li>";
+                    echo $valeur[5] ? "<li>numéro de téléphone affiché</li>" : "<li > numéro de téléphone non affiché</li>"; ?>
 
 
-            <input onclick="changeDisplay(<?=$i?>)" type="button"  value="-" id=<?="btnChangeDisplay".$i?>>
+                </div>
+                <script src="../Controller/JsDisplayDashBoard.js"></script>
 
 
+                <input onclick="changeDisplay(<?= $i ?>)" type="button" class="btnChangeDisplay" value="-"
+                       id=<?= "btnChangeDisplay" . $i ?>>
 
 
-            <form >
-                 <!-- mettre en action la fonction supprimer -->
+                <form method="post" action="../Controller/ControllerAfficheTableau.php">
+                    <!-- mettre en action la fonction supprimer -->
 
-                <button>supprimer</button>
+                    <button id="<?= "delete" . $i ?>" class="btnDelete" type="submit" onclick="afficherAlerte()">supprimer</button>
+                    <input type="hidden" value="<?= $valeur[1] ?>" name="idDashboard">
+                    <!-- mettre input hidden + validation -->
+                    <!-- mettre en action la fonction modifier -->
 
-                <!-- mettre en action la fonction modifier -->
-
-
-                <button id="show"> afficher tableau de bord</button>
                 </form>
 
-            <form>
-                <input type="button" value="modifier" class="modif" id=<?=$i?> >
-            </form>
-        </div>
+                <form action="">
+                    <button id="<?= "show" . $i ?>" type="submit" class="btnShow"> afficher tableau de bord</button>
+                </form>
 
-<br>
-<?php
-}
-}
-?>
+                <form action="PageModifDashBoard.php">
+                    <button type="submit" value="modifier" class="modif" id="<?= $i ?>" class="btnModif"> modifier
+                    </button>
+                </form>
+            </div>
+
+            <br>
+            <?php
+        }
+    }
+    ?>
 
 </section>
-<form action="../View/PageCreationTableau.php">
-    <button>Ajouter un tableau de bord</button>
-</form>
+
 <footer class="bottomBanner">
     <div class="nomFooter">
         <p>

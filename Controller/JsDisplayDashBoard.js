8fb2dc3f2a3bc83c75805c482c9be122159e4741
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-12-18 16:44:42 +0100

    Ajout d'une condition pour empécher de supprimer des tableau de bord

--- Controller/JsDisplayDashBoard.js
+++ Controller/JsDisplayDashBoard.js
@@ -6,7 +6,18 @@
  * try to show or hide the element in the div elementCacher (nom provisiore)
  */
 
+function afficherAlerte() {
+    var resultat = window.confirm("Voulez-vous continuer ?");
 
+    // Vérifier le résultat de l'alerte
+    if (resultat) {
+        // L'utilisateur a cliqué sur "OK"
+        alert("Vous avez choisi de continuer.");
+    } else {
+        // L'utilisateur a cliqué sur "Annuler"
+        alert("Vous avez choisi d'annuler.");
+    }
+}
 
 function changeDisplay(id){
     var divCacher = document.getElementById(id);

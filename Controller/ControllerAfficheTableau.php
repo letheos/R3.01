f90028151ca20df3a1c3b837b5b61396fa8b315a
Rafale2000 <120012541+Rafale2000@users.noreply.github.com>
2023-12-18 16:42:48 +0100

    on peut effacer des tableau de bord cependant ils s'effaces tout les temps

--- Controller/ControllerAfficheTableau.php
+++ Controller/ControllerAfficheTableau.php
@@ -1,12 +1,53 @@
 <?php
+//TODO efface quand meme si tu dis non
+if (!isset($_SESSION['login'])) {
+    $_SESSION['login'] = "user1";
+}
+$User = $_SESSION['login'];
+
 require "../Model/ModelSelect.php";
+require "../Model/ModelInsertUpdateDelete.php";
+
+
 $conn = require "../Model/Database.php";
 
-function ControllerGetDashBoardPerUser($login){
-    $conn = require "../Model/Database.php";
-    return getDashBoardPerUser($login,$conn);
-}
-function ControllerGetYearWitDashBoard($idDashboard){
-    $conn = require "../Model/Database.php";
 
+if (isset($_POST['idDashboard'])) {
+    //echo '<script>alert("voulez vous supprimer ce tableau de bord ?")</script>';
+    $idDashboardForDelette = $_POST['idDashboard'];
+    echo $_POST['idDashboard'];
+    ControllerDeleteDashBoard($idDashboardForDelette, $_SESSION['login']);
+
+    header('location:../View/PageAfficheTableau.php');
+}
+
+/**
+ * @param $login String
+ * @return String[]
+ */
+function ControllerGetDashBoardPerUser($login)
+{
+    $conn = require "../Model/Database.php";
+    return getDashBoardPerUser($login, $conn);
+}
+
+/**
+ * @param $idDashboard int
+ * @return void
+ */
+function ControllerGetYearWitDashBoard($idDashboard)
+{
+    $conn = require "../Model/Database.php";
+}
+
+/**
+ * @param $idDashboard int
+ * @param $login String
+ * @return void
+ */
+function ControllerDeleteDashBoard($idDashboard, $login)
+{
+    echo '<script>alert("controler")</script>';
+    $conn = require "../Model/Database.php";
+    deleteUserDashBoard($login, $idDashboard, $conn);
 }

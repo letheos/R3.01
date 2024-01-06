3c510e51cbcbbdaa491e1b7f7857f4803262ac1b
rafale2000 <loick1morneau@icloud.com>
2024-01-06 17:45:03 +0100

    ajout de la fonction checked

--- Controller/JsDisplayDashBoard.js
+++ Controller/JsDisplayDashBoard.js
@@ -37,3 +37,21 @@ function changeDisplay(id){
     }
 }
 
+function checked(isPermis,isIne,isAddress,isPhone,id){
+    var ineShow = document.getElementById('ine');
+    var addressShow =  document.getElementById('address');
+    var phoneShow = document.getElementById('phone');
+    var permisShow = document.getElementById('permis');
+    if(isPermis){
+        permisShow.checked = true;
+    } if(isIne){
+        ineShow.checked = true;
+    } if(isPhone){
+        phoneShow.checked = true;
+    } if(isAddress){
+        permisShow.checked = true;
+    }
+
+
+}
+

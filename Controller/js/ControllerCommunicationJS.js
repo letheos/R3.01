9634a9020682c43016ad586041d1f805ee76f2f7
Timothee <timothee.allix@gmail.com>
2024-01-10 15:21:18 +0100

    Proto Css

--- Controller/js/ControllerCommunicationJS.js
+++ Controller/js/ControllerCommunicationJS.js
@@ -16,19 +16,14 @@ function modifOff(Id){
 }
 
 function checkIfModifierHidden() {
-    // Récupérer tous les éléments avec la classe "buttonSubmit"
     var elements = document.querySelectorAll('.buttonSubmit');
-
-    // Parcourir tous les éléments pour vérifier si un élément "Modifier" est en mode masqué
     for (var i = 0; i < elements.length; i++) {
         var modifier = elements[i].querySelector("#Modify");
         if (modifier && modifier.style.display === "none") {
-            // Il y a un élément "Modifier" en mode masqué
             return true;
         }
     }
 
-    // Aucun élément "Modifier" n'est en mode masqué
     return false;
 }
 function isContentAnImage(Id) {
@@ -42,20 +37,14 @@ function transformToTextarea(Id) {
     var messageDiv = document.getElementById(Id);
 
     if (messageDiv !== null && !checkIfModifierHidden() && !isContentAnImage(Id)) {
-        // Enregistre le texte original dans une variable
         var originalText = messageDiv.innerText;
-
         var textarea = document.createElement('textarea');
         textarea.name = 'la';
         textarea.value = originalText;
         textarea.id='letexte';
-
-        // Ajoute un champ hidden avec le texte original
         var originalHidden = document.createElement('input');
         originalHidden.type = 'hidden';
         originalHidden.name = 'la';
-
-        // Replace le contenu existant avec la textarea et le champ hidden
         messageDiv.innerHTML = '';
 
         messageDiv.appendChild(textarea);
@@ -71,15 +60,9 @@ function transformToTextarea(Id) {
 function executerFormulaire(Id) {
     var texte = document.getElementById('letexte').value;
     var formulaire = document.getElementById(Id);
-
-    // Récupérer le champ hidden "la" dans le contexte du formulaire
     var originalHidden = formulaire.querySelector('input[name="la"]');
-
-    // Mettre à jour la valeur du champ hidden avec le texte modifié
     if (originalHidden) {
         originalHidden.value = texte;
     }
-
-    // Soumettre le formulaire
     formulaire.submit();
 }
--- View/PageCommunication.php
+++ View/PageCommunication.php
@@ -28,7 +28,7 @@ if(isset($_SESSION['year'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="StylePageAlerte.css">
+    <link rel="stylesheet" href="StylePageCommunication.css">
     <title>Echange</title>
 </head>
 <body>

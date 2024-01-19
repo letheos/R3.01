6eb16b925cf96fa2cb62732728b7029b73dc509a
timothee <timothee.allix@gmail.com>
2024-01-20 00:48:18 +0100

    Passe le 1er insert avec les bonnes valeurs, il faut maintenant regrouper certaines des valeurs dans un tableau pour passer les suivants (+ probleme 1er insert, au lieu de recuperer Parcours A - GEII, il va recuperer Parcours A -- (un grand trait, donc caractere different) GEII)

--- Controller/csv.php
+++ Controller/csv.php
@@ -2,7 +2,6 @@
 $conn = require "../Model/database.php";
 require "../Model/ModelInsertUpdateDelete.php";
 
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -52,94 +51,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $indexProject = array_search("Projet d'entreprise", $entete);
            // if ($indexINE !== false && $indexNom !== false && $indexPrenom !== false) {
                 echo '<script>alert("après if")</script>';
-                while (($ligne = fgetcsv($fichier, 0, ';')) !== false) {
+                while (($ligne = fgetcsv($fichier, 0, ',')) !== false) {
+                    $ine = $ligne[0];
+                    $nom = $ligne[1];
+                    $prenom = $ligne[2];
+                    $mail = $ligne[3];
+                    $alternance = $ligne[4];
+                    $parcours = $ligne[5];
+                    $year = $ligne[8];
+                    $phone = $ligne[49];
+                    $city = $ligne[40];
+                    $postal = $ligne[41];
+                    $namestreet = $ligne[44];
+                    $numstreet = $ligne[43];
+                    $project = $ligne[21];
+                    echo "INE: $ine<br>";
+                    echo "Nom: $nom<br>";
+                    echo "Prénom: $prenom<br>";
+                    echo "Adresse e-mail: $mail<br>";
+                    echo "Alternance: $alternance<br>";
+                    echo "Nom du parcours: $parcours<br>";
+                    echo "Année du parcours: $year<br>";
+                    echo "Téléphone: $phone<br>";
+                    echo "Ville: $city<br>";
+                    echo "Code Postal: $postal<br>";
+                    echo "Nom de Rue: $namestreet<br>";
+                    echo "Numéro de Rue: $numstreet<br>";
+                    echo "Projet d'Entreprise: $project<br>";
 
-                    $ine = $ligne[$indexINE];
-                    $nom = $ligne[$indexNom];
-                    $prenom = $ligne[$indexPrenom];
-                    $mail = $ligne[$indexMail];
-                    $alternance = $ligne[$indexAlt];
-                    $parcours = $ligne[$indexParcours];
-                    $year = $ligne[$indexYear];
-                    $phone = $ligne[$indexPhone];
-                    $city = $ligne[$indexCity];
-                    $postal = $ligne[$indexPostal];
-                    $namestreet = $ligne[$indexNameStreet];
-                    $numstreet = $ligne[$indexNumberStreet];
-                    $project = $ligne[$indexProject];
-                        /*
-                        $nameFormation = $ineParts[7];
-                        $yearFormationStart = $ineParts[8];
-                        $etablissement = $ineParts[9];
-                        $ecole = $ineParts[10];
-                        $genre = $ineParts[11];
-                        $dateNaissance = $ineParts[12];
-                        $nomNaissance = $ineParts[13];
-                        $lieuNaissance = $ineParts[14];
-                        $codeDepartementNaissance = $ineParts[15];
-                        $nationalite = $ineParts[16];
-                        $situationFamiliale = $ineParts[17];
-                        $handicap = $ineParts[18];
-                        $rqth = $ineParts[19];
-                        $numSecuSociale = $ineParts[20];
-                        $projetEntreprise = $ineParts[21];
-                        $boursierSecondaire = $ineParts[22];
-                        $boursierSuperieur = $ineParts[23];
-                        $codeDepartementPrecedentEtablissement = $ineParts[24];
-                        $nomDernierDiplome = $ineParts[27];
-                        $codeDernierDiplome = $ineParts[28];
-                        $nomDernierDiplomePrepare = $ineParts[29];
-                        $codeDernierDiplomePrepare = $ineParts[30];
-                        $nomDerniereClasse = $ineParts[31];
-                        $codeDerniereClasse = $ineParts[32];
-                        $nomDerniereSituation = $ineParts[33];
-                        $codeDerniereSituation = $ineParts[34];
-                        $nomSituationAnneePrecedente = $ineParts[35];
-                        $codeApprentiSituationAnneePrecedente = $ineParts[36];
-                        $autreCodeSituationAnneePrecedente = $ineParts[37];
-                        $prenomRepresentantLegal = $ineParts[38];
-                        $nomRepresentantLegal = $ineParts[39];
-                        $emailRepresentantLegal = $ineParts[40];
-                        $paysAdressePersonnelle = $ineParts[41];
-                        $villeAdressePersonnelle = $ineParts[42];
-                        $codePostalAdressePersonnelle = $ineParts[43];
-                        $codeInseeAdressePersonnelle = $ineParts[44];
-                        $numRueAdressePersonnelle = $ineParts[45];
-                        $nomRueAdressePersonnelle = $ineParts[46];
-                        $localiteAdressePersonnelle = $ineParts[47];
-                        $batimentAdressePersonnelle = $ineParts[48];
-                        $appartementAdressePersonnelle = $ineParts[49];
-                        $telFixeAdressePersonnelle = $ineParts[50];
-                        $telPortableAdressePersonnelle = $ineParts[51];
-                        $faxAdressePersonnelle = $ineParts[52];
-                        $paysAdresseResidentielle = $ineParts[53];
-                        $villeAdresseResidentielle = $ineParts[54];
-                        $codePostalAdresseResidentielle = $ineParts[55];
-                        $codeInseeAdresseResidentielle = $ineParts[56];
-                        $numRueAdresseResidentielle = $ineParts[57];
-                        $nomRueAdresseResidentielle = $ineParts[58];
-                        $localiteAdresseResidentielle = $ineParts[59];
-                        $batimentAdresseResidentielle = $ineParts[60];
-                        $appartementAdresseResidentielle = $ineParts[61];
-                        $telFixeAdresseResidentielle = $ineParts[62];
-                        $telPortableAdresseResidentielle = $ineParts[63];
-                        $faxAdresseResidentielle = $ineParts[64];
-                        $paysAdresseRepresentantLegal = $ineParts[65];
-                        $villeAdresseRepresentantLegal = $ineParts[66];
-                        $codePostalAdresseRepresentantLegal = $ineParts[67];
-                        $codeInseeAdresseRepresentantLegal = $ineParts[68];
-                        $numRueAdresseRepresentantLegal = $ineParts[69];
-                        $nomRueAdresseRepresentantLegal = $ineParts[70];
-                        $localiteAdresseRepresentantLegal = $ineParts[71];
-                        $batimentAdresseRepresentantLegal = $ineParts[72];
-                        $appartementAdresseRepresentantLegal = $ineParts[73];
-                        $telFixeAdresseRepresentantLegal = $ineParts[74];
-                        $telPortableAdresseRepresentantLegal = $ineParts[75];
-                        $faxAdresseRepresentantLegal = $ineParts[76];
-*/
+
 // Constructing the full address for the 'Adresse personnelle'
                         $addressStreet = $numstreet . ' ' . $namestreet;
-                    echo "INE: $ine, Nom: $nom, Prenom: $prenom, Mail: $mail, Alternance: $alternance, Parcours: $parcours, Année: $year, Téléphone: $phone, Ville: $city, Code Postal: $postal, Nom de Rue: $namestreet, Numéro de Rue: $numstreet, Projet d'Entreprise: $project";
+                    //echo "INE: $ine, Nom: $nom, Prenom: $prenom, Mail: $mail, Alternance: $alternance, Parcours: $parcours, Année: $year, Téléphone: $phone, Ville: $city, Code Postal: $postal, Nom de Rue: $namestreet, Numéro de Rue: $numstreet, Projet d'Entreprise: $project";
                     if($ine != ""){
 // Now you can use these variables in your insertCandidate function
                      insertCandidate($conn,$ine,$nom,$prenom,$year,$mail,$phone,$parcours,0,'azz',$project,$addressStreet,null,null);

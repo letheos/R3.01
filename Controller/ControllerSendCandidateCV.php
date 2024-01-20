34c4d7e7c4af2fba4f299ffcf77116755441cd3a
timothee <timothee.allix@gmail.com>
2024-01-20 16:35:04 +0100

    Test archives pour les cv

--- Controller/ControllerSendCandidateCV.php
+++ Controller/ControllerSendCandidateCV.php
@@ -34,20 +34,62 @@ function sendEmail($conn, $from, $to, $msg, $infos) {
     }
 }
 
+function dlArchive($infos)
+{
+    global $conn;
+    $val = array();
+    foreach ($infos as $candidat) {
+        $val[] = selectCandidatById($candidat);
+    }
+    $nom = "cv".str(date());
+    $archivePath = createImageArchive($conn,$val,$nom);
+    header('Content-Type: application/zip');
+    header('Content-Disposition: attachment; filename="' . "test" . '"');
+    header('Content-Length: ' . filesize($archivePath));
+    readfile($archivePath);
+
+    unlink($archivePath);
+}
+
+function createImageArchive($conn,$userandcv, $outputArchiveName) {
+
+
+    // Créer un objet ZipArchive
+    $zip = new ZipArchive();
+    if ($zip->open($outputArchiveName, ZipArchive::CREATE) !== TRUE) {
+        die ("Impossible de créer l'archive");
+    }
+
+    // Parcourir les résultats et ajouter chaque image à l'archive
+    foreach ($userandcv as $val){
+        $imageId = $val['name'].$val['firstName'];
+        $imagePath = $val['cv'];
+
+        // Télécharger l'image depuis le chemin dans la base de données
+        $imageContent = file_get_contents($imagePath);
+
+        // Ajouter l'image à l'archive avec un nom de fichier unique (par exemple, basé sur l'ID)
+        $zip->addFromString("cv_$imageId.jpg", $imageContent);
+    }
+
+    $zip->close();
+    return $outputArchiveName;
+}
 
 $to = $_POST['to'];
 
 $success = 1;
 
 if(!empty($_POST['candidateCheckbox'])) {
-    if (!empty($to)){
+    /*if (!empty($to)){
         $msg = "Mail envoyée avec succés";
         sendEmail($conn, $mail, $to, $_POST['message'], $_POST['candidateCheckbox']);
     } else {
         $msg = "Il manque le destinataire";
         $success = 0;
     }
-
+    */
+    dlArchive($_POST['candidateCheckbox']);
 } else {
     $msg = "Vous n'avez pas selectionné de CV";
     $success = 0;

3589175c05d112267758593e3c64b4b3eb39a0ce
Nathan Strady <nathan.strady.tra@gmail.com>
2024-01-21 17:10:32 +0100

    Correction d'alerte du jour

---
+++ Controller/ControllerGestionArchive.php
@@ -0,0 +1,45 @@
+<?php
+
+
+function dlArchive($infos)
+{
+    global $conn;
+    $val = array();
+    foreach ($infos as $candidat) {
+        $val[] = selectCandidatById($conn,$candidat);
+    }
+    $archivePath = createImageArchive($val,"cv.zip");
+    if (file_exists($archivePath)) {
+        ob_end_clean();
+        header('Content-Type: application/zip');
+        header('Content-Disposition: attachment; filename="cvs.zip"');
+        header('Content-Length: ' . filesize($archivePath));
+
+        readfile($archivePath);
+
+        unlink($archivePath);
+    } else {
+        die("Impossible de trouver l'archive.");
+    }
+}
+
+function createImageArchive($userandcv, $outputArchiveName) {
+
+    global $conn;
+    $zip = new ZipArchive();
+    if ($zip->open($outputArchiveName, ZipArchive::CREATE) !== TRUE) {
+        die ("Impossible de créer l'archive");
+    }
+
+    foreach ($userandcv as $val){
+        if($val['cv']!= null and $val['cv']!="") {
+            $imageId = $val['name'] . $val['firstName']."." . pathinfo($val['cv'], PATHINFO_EXTENSION);
+            $imagePath = $val['cv'];
+            echo $imageId . ":" . $imagePath;
+            $zip->addFile($imagePath,$imageId);
+        }
+    }
+
+    $zip->close();
+    return $outputArchiveName;
+}
